b2ff66b28fc4025b0f0c08d94d13a927bf47cced
Sepa123 <sebastian.retamal.contreras@gmail.com>
2023-01-26 19:22:57 -0300

    Update youtubeDownload.py

--- youtubeDownload.py
+++ youtubeDownload.py
@@ -1,16 +1,13 @@
 import pytube
 
-## Descarga el audio de un video de YouTube
-
 ytUrl = input("ingrese una URL de un video de YouTube: ")
 
 ytVideo = pytube.YouTube(ytUrl)
 print("Descargando Video ...")
-video = ytVideo.streams.get_by_resolution("720p") # para resolucion de 720p
+video = ytVideo.streams.get_by_resolution("720p") 
 # ytVideo.streams.get_highest_resolution() # para obtener la max resolucion posible
 
 video.download()
 
 print("video descargado ...")
 
-# https://www.youtube.com/watch?v=_y9qQZXE24A&t=36sS

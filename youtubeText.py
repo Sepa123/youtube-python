273f0e30d7535761f2564c464a418ff9c93e33e5
Sepa123 <sebastian.retamal.contreras@gmail.com>
2023-01-26 19:20:45 -0300

    Initial commit

---
+++ youtubeText.py
@@ -0,0 +1,54 @@
+import pytube
+import whisper
+
+from fpdf import FPDF
+import os
+
+## Descarga el audio de un video de YouTube
+
+ytUrl = input("ingrese una URL de un video de YouTube: ")
+
+ytVideo = pytube.YouTube(ytUrl)
+titleAudio = ytVideo.title.replace(" ","_")
+
+audio = ytVideo.streams.get_audio_only()
+print("Descargando Audio ...")
+audio.download(filename=titleAudio + ".mp4")
+
+model = whisper.load_model("small")
+print("Trascribiendo ...")
+result = model.transcribe(titleAudio + ".mp4", fp16=False)
+
+print("Transcripción completada")
+
+print("Escribiendo el texto de audio en formato .pdf")
+
+#Convertir a pdf
+
+pdf = FPDF()
+
+pdf.add_page()
+
+pdf.set_font("Arial", size = 14)
+pdf.cell(200,10,txt = ytVideo.title, align= "C")
+pdf.ln()
+pdf.set_font("Arial", size = 12)
+pdf.multi_cell(0,5,txt = result["text"], border = 0, align= "J")
+
+pdf.output(titleAudio +".pdf")
+
+
+# eliminando audio
+
+os.remove(titleAudio + ".mp4")
+
+
+# #Convertir a txt
+
+# print("Escribiendo en texto de audio en formato .txt")
+# txt = open(titleAudio  + ".txt", "w")
+# txt.write(result["text"])
+
+# txt.close()
+
+# https://www.youtube.com/watch?v=hZX05MQxgTg
